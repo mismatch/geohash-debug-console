41ce613d1c435a66b08d1ed2afa9e1def8ee750d
mismatch <mismatch@yandex.ru>
2015-08-18 14:26:41 -0400

    refactored console.js

--- src/main/resources/www/js/console.js
+++ src/main/resources/www/js/console.js
@@ -1,24 +1,53 @@
-(function() {
-	var map = L.map('map').setView([49.977, 20.027], 12);
-	var marker = null;
-	map.on('click', onMapClick);
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
+(function(debugParams) {
 
-	function onMapClick(e) {
-		var coords = e.latlng;
-		if (null == marker) {
-			marker = L.marker(coords).addTo(map);
-		} else {
-			marker.setLatLng(coords).update();
+	var view = {
+		map: L.map("map"),
+		marker: null,
+		debugLine: {
+			pointLabel: $("#pointLabel"),
+			hashLabel: $("#hashLabel")
 		}
+	};
 
-		$("#point").text("(" + coords.lat + ", " + coords.lng + ")");
-		
-		$.getJSON("/hashes/point?bits=32&lat=" + coords.lat + "&lng=" + coords.lng, 
-				function(data) {
-					$("#geohash").text(data.geohash);
-				});
-	}
-})();
+	var controller = {
+		onLocationChange: function(ev) {
+			var point = ev.latlng;
+			debugParams.lat = point.lat;
+			debugParams.lng = point.lng;
+
+			controller.updateMarker(point);
+			controller.updatePointLabel(point);
+			controller.requestHashData();
+		},
+
+		requestHashData: function() {
+			$.getJSON("/hashes/point?" + $.param(debugParams), controller.whenHashReceived);
+		},
+
+		whenHashReceived: function(hashData) {
+			view.debugLine.hashLabel.text(hashData.geohash);
+		},
+
+		updateMarker: function(point) {
+			view.marker.setLatLng(point).update();
+		},
+
+		updatePointLabel: function(point) {
+			view.debugLine.pointLabel.text("(" + point.lat + ", " + point.lng + ")");
+		},
+
+		initView: function() {
+			var mapCenter = [debugParams.lat, debugParams.lng];
+			view.map.setView(mapCenter, 12);
+			view.marker = L.marker(mapCenter).addTo(view.map);
+			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				}).addTo(view.map);
+			view.map.on('click', controller.onLocationChange);
+			controller.onLocationChange({latlng: debugParams});
+		}
+	};
+
+	controller.initView();
+
+})({lat:49.977, lng: 20.027, bits: 32});
