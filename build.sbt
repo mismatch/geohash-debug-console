9c203325f809d8ef781974299221f53cdc7a307d
mismatch <mismatch@yandex.ru>
2017-02-11 13:22:52 +0000

    Updated dependencies

--- build.sbt
+++ build.sbt
@@ -3,18 +3,18 @@ description := "Web-based debug console to poke around geohash and related thing
 
 version := "0.1.1-SNAPSHOT"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.12.1"
 
 mainClass in Compile := Some("mismatch.geohash.console.Server")
 assemblyJarName in assembly := s"${name.value}-dist.jar"
 
 libraryDependencies ++= Seq(
-  "net.databinder" %% "unfiltered-directives" % "0.8.4",
-  "net.databinder" %% "unfiltered-filter" % "0.8.4",
-  "net.databinder" %% "unfiltered-jetty" % "0.8.4",
-  "net.databinder" %% "unfiltered-json4s" % "0.8.4",
-  "net.databinder" %% "unfiltered-scalatest" % "0.8.4" % "test",
-  "ch.hsr" % "geohash" % "1.1.0"
+  "ws.unfiltered" %% "unfiltered-directives" % "0.9.0",
+  "ws.unfiltered" %% "unfiltered-filter" % "0.9.0",
+  "ws.unfiltered" %% "unfiltered-jetty" % "0.9.0",
+  "ws.unfiltered" %% "unfiltered-json4s" % "0.9.0",
+  "ws.unfiltered" %% "unfiltered-scalatest" % "0.9.0" % "test",
+  "ch.hsr" % "geohash" % "1.3.0"
 )
 
 resolvers ++= Seq(
--- src/test/scala/GeohashDebugConsoleTest.scala
+++ src/test/scala/GeohashDebugConsoleTest.scala
@@ -1,49 +1,49 @@
 package mismatch.geohash.console
 
-import org.scalatest.matchers.ShouldMatchers
-import org.scalatest.{GivenWhenThen, FeatureSpec}
+import org.scalatest.{Matchers, GivenWhenThen, FeatureSpec}
 import unfiltered.scalatest.Hosted
 import unfiltered.scalatest.jetty.Served
-import org.apache.http.util.EntityUtils
+import okhttp3._
 
-class GeohashDebugConsoleTest extends FeatureSpec with Served with Hosted with GivenWhenThen with ShouldMatchers {
-  import dispatch.classic._
+class GeohashDebugConsoleTest extends FeatureSpec with Served with Hosted with GivenWhenThen with Matchers {
 
   override val port = 8080
 
   def setup = {    
-    _.filter(new GeohashDebugConsole)
+    _.plan(new GeohashDebugConsole)
   }
 
   feature("rest service") {
-    val http = new Http
+    val http = new OkHttpClient()
 
     scenario("should validate coordinates") {
-      given("invalid latitude and longitude parameters")
-        val srvUrl = url("http://localhost:8080/hashes/point?bits=7&lat=96.333&lng=211.336")
-      when("request has been made")
-        val (status, body) = http.x(srvUrl) {
-            case (code, _, Some(body)) => (code, EntityUtils.toString(body))
-        }
-      then("status is Bad Request")
+      Given("invalid latitude and longitude parameters")
+        val request = new Request.Builder()
+          .url("http://localhost:8080/hashes/point?bits=7&lat=96.333&lng=211.336")
+          .build()
+      When("request has been made")
+        val response = http.newCall(request).execute()
+        val status = response.code
+        val body = response.body().string()
+      Then("status is Bad Request")
         status should be(400)
-      and("""body includes "is not correct value of latitude" """)
+      And("""body includes "is not correct value of latitude" """)
         body should include("is not correct value of latitude")
-      and ("""body includes "is not correct value of longitude" """)
+      And ("""body includes "is not correct value of longitude" """)
         body should include("is not correct value of longitude")
     }
 
     scenario("should validate number of bits") {
-      given("invalid number of bits")
-        val srvUrl = url("http://localhost:8080/hashes/point?bits=77&lat=36.333&lng=11.336")
-      when("request has been made")
-        val (status, body) = http.x(srvUrl) {
-            case (code, _, Some(body)) => (code, EntityUtils.toString(body))
-        }
-      then("status is Bad Request")
-        status should be(400)
-      and("""body includes "is out of bits range" """)
-        body should include("is out of bits range")
+      Given("invalid number of bits")
+        val request = new Request.Builder()
+          .url("http://localhost:8080/hashes/point?bits=77&lat=36.333&lng=11.336")
+          .build()
+      When("request has been made")
+        val response = http.newCall(request).execute()        
+      Then("status is Bad Request")
+        response.code should be(400)
+      And("""body includes "is out of bits range" """)
+        response.body().string() should include("is out of bits range")
     }
   }
 }
